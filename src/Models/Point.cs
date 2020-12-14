9315b4736d98d8da41cf611e8201d0fc7299e7ee
Rik Harink <rik.harink@topicus.nl>
2020-12-14 09:53:48 +0100

    Day 12

--- src/Models/Point.cs
+++ src/Models/Point.cs
@@ -1,17 +1,53 @@
-﻿namespace AOC2020.Models
-{
-    public record Point(int X, int Y)
-    {
-        public static implicit operator Point((int, int) point) => new Point(point.Item1, point.Item2);
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        public static Point operator +(Point a, Point b)
+namespace AOC2020.Models
+{
+    [DebuggerDisplay("(x: {X}, y: {Y})")]
+    public readonly struct Point : IEquatable<Point>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
         {
-            return new Point(a.X + b.X, a.Y + b.Y);
+            X = x;
+            Y = y;
         }
 
+        public static implicit operator Point((int x, int y) point)
+            => new Point(point.x, point.y);
+
+        public static Point operator +(Point a, Point b)
+            => new Point(a.X + b.X, a.Y + b.Y);
+
         public static Point operator -(Point a, Point b)
+            => new Point(a.X - b.X, a.Y - b.Y);
+
+        public static Point operator *(Point a, Point n)
+            => new Point(a.X * n.X, a.Y * n.Y);
+
+        public static Point operator *(Point a, int n)
+          => new Point(a.X * n, a.Y * n);
+
+        public static Point operator /(Point a, int n)
+          => new Point(a.X / n, a.Y / n);
+        public static bool operator ==(Point left, Point right) => Equals(left, right);
+        public static bool operator !=(Point left, Point right) => !Equals(left, right);
+        public int Dot(Point other) => Dot(this, other);
+        public static int Dot(Point a, Point b) => (a.X * b.X) + (a.Y * b.Y);
+        public override bool Equals(object? obj) => (obj is Point vector) && Equals(vector);
+        public override int GetHashCode() => (X, Y).GetHashCode();
+        public bool Equals(Point other) => (X, Y) == (other.X, other.Y);
+        public override string ToString() => $"({X}, {Y})";
+
+        public void Deconstruct(out double x, out double y)
         {
-            return new Point(a.X - b.X, a.Y - b.Y);
+            (x, y) = (X, Y);
         }
     }
 }
