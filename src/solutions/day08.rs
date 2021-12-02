185276575dbc901e937633acaaac7fa83e67c23c
Rik Harink <Rik.Harink@topicus.nl>
2021-12-02 12:12:11 +0100

    default succes for template day setup

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -14,14 +14,14 @@ fn test_input() -> Vec<String> {
 
 #[test]
 fn test_part_one() {
-    let expected = 7;
+    let expected = 0;
     let actual = part_one(&test_input());
     assert_eq!(expected, actual);
 }
 
 #[test]
 fn test_part_two() {
-    let expected = 5;
+    let expected = 0;
     let actual = part_two(&test_input());
     assert_eq!(expected, actual);
 }
