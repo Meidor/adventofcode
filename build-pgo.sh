1107e33e9e7b493cffc2f0d3aeedcde9c381359a
Rik Harink <rik@har.ink>
2021-12-12 14:52:55 +0100

    PGO build

---
+++ build-pgo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+rm -rf /tmp/pgo-data
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" cargo build --release --target=x86_64-unknown-linux-gnu
+./target/x86_64-unknown-linux-gnu/release/adventofcode all
+./target/x86_64-unknown-linux-gnu/release/adventofcode all
+./target/x86_64-unknown-linux-gnu/release/adventofcode all
+~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
+RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata" cargo build --release --target=x86_64-unknown-linux-gnu
