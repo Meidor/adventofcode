2d1db4f8fe647e707cc9fd4c3c8527b7a130d916
Rik Harink <rik@har.ink>
2021-12-12 15:20:37 +0100

    more tweaks to workflow

--- build-pgo.sh
+++ build-pgo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 rm -rf /tmp/pgo-data
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" cargo build --release --target=x86_64-unknown-linux-gnu
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data -Ctarget-cpu=native" cargo build --release --target=x86_64-unknown-linux-gnu
 ./target/x86_64-unknown-linux-gnu/release/adventofcode all
 ./target/x86_64-unknown-linux-gnu/release/adventofcode all
 ./target/x86_64-unknown-linux-gnu/release/adventofcode all
 ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
-RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata" cargo build --release --target=x86_64-unknown-linux-gnu
+RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata -Ctarget-cpu=native" cargo build --release --target=x86_64-unknown-linux-gnu
